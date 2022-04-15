fbe171fda6b271ca467d32bec34660b54d40c640
askild85 <askildar@yandex.ru>
2022-04-15 15:20:10 +0500

    Выгрузка списка в Эксель

--- src/app/ip-list/ip-list.component.ts
+++ src/app/ip-list/ip-list.component.ts
@@ -1,16 +1,90 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { AbstractControl, FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { LocationInfo } from './../model/IpInfo';
 import { HttpService } from './../services/http.service';
-
+import * as xlsx from 'xlsx';
 @Component({
   selector: 'app-ip-list',
   templateUrl: './ip-list.component.html',
   styleUrls: ['./ip-list.component.scss']
 })
 export class IpListComponent {
-  ipList: LocationInfo[] = []
+  ipList: LocationInfo[] = [
+    {
+      ip: "8.8.8.8",
+      continent: "North America",
+      country: "United States",
+      country_capital: "Washington D.C."
+    },
+    {
+      "ip": "8.8.8.89",
+      "continent": "North America",
+      "country": "United States",
+      "country_capital": "Washington D.C."
+    },
+    {
+      "ip": "128.8.8.89",
+      "continent": "North America",
+      "country": "United States",
+      "country_capital": "Washington D.C."
+    },
+    {
+      "ip": "128.8.128.89",
+      "continent": "North America",
+      "country": "United States",
+      "country_capital": "Washington D.C."
+    }
+  ]
   errorMsg!: string;
+  clubs = [
+    {
+      position: 1,
+      name: "Liverpool",
+      played: 20,
+      won: 19,
+      drawn: 1,
+      lost: 0,
+      points: 58
+    },
+    {
+      position: 2,
+      name: "Leicester City",
+      played: 21,
+      won: 14,
+      drawn: 3,
+      lost: 4,
+      points: 45
+    },
+    {
+      position: 3,
+      name: "Manchester City",
+      played: 21,
+      won: 14,
+      drawn: 2,
+      lost: 5,
+      points: 44
+    },
+    {
+      position: 4,
+      name: "Chelsea",
+      played: 21,
+      won: 11,
+      drawn: 3,
+      lost: 7,
+      points: 36
+    },
+    {
+      position: 5,
+      name: "Manchester United",
+      played: 21,
+      won: 8,
+      drawn: 7,
+      lost: 6,
+      points: 31
+    }
+  ];
+  @ViewChild('epltable', { static: false }) epltable!: ElementRef;
+
   addForm : FormGroup = new FormGroup({
     ip: new FormControl('8.8.8.8', [Validators.required, this.customValidator()]),
   })
@@ -33,6 +107,8 @@ export class IpListComponent {
       },
       error: (err) => console.log(err),
     });
+    console.log(this.ipList);
+
   }
 
   customValidator() {
@@ -50,4 +126,15 @@ export class IpListComponent {
   remove(ip: string) {
     this.ipList = this.ipList.filter(data => data.ip !== ip)
   }
+
+  exportToExcel() {
+    const ws: xlsx.WorkSheet =
+    xlsx.utils.table_to_sheet(this.epltable.nativeElement);
+
+    const wb: xlsx.WorkBook = xlsx.utils.book_new();
+
+    xlsx.utils.book_append_sheet(wb, ws, 'Sheet1');
+    xlsx.writeFile(wb, 'epltable.xlsx');
+  }
 }
+
